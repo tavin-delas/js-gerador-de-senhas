68c7dc83758bda7a9696d8397dbfb0eff019eceb
tavin-delas <luiz.machado.cruz@escola.pr.gov.br>
2025-09-05 09:11:35 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const numeroSenha = document.querySelector('parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha < 20){  
+        //tamanhoSenha = tamanhoSenha + 1
+        tamanhoSenha++;                                          
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
